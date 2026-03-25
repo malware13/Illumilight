b01a1e7bacce42c9165856cbcbdc284df708ad82
malware13 <79044410+malware13@users.noreply.github.com>
2026-03-26 00:41:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="ILLUMILIGHT — Social Enterprise",
-    page_icon="💡",
+    page_icon="assets/logo.ico",  # ← path to your icon file
     layout="wide",
     initial_sidebar_state="collapsed"
 )
